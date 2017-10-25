820ab55e90e4318cd209d12171f357af2dfb8cab
Maxwell Russell <maxruss.87@gmail.com>
2017-10-25 14:30:13 -0500

    updating ignore files

--- app/src/creds.js
+++ app/src/creds.js
@@ -3,12 +3,12 @@
 // pool-id
 
 const creds = {
-  'user-pool-id' : 'us-east-2_7v5hHewEg',
-  'identity-pool-id' : 'us-east-2:a175250c-dbc0-44fb-8bf5-388fe6c554fb',
-  'app-client-id': '3pefe735qu5hopvpt7qp5lm19',
-  'test-user-id' : '9d4702dc-e28d-4248-800a-03f093bec937',
-  'region' : 'us-east-2',
-  'invoke-url' : 'https://3saadq97v8.execute-api.us-east-2.amazonaws.com/prod'
+  user_pool_id : 'us-east-2_7v5hHewEg',
+  identity_pool_id : 'us-east-2:a175250c-dbc0-44fb-8bf5-388fe6c554fb',
+  app_client_id: '3pefe735qu5hopvpt7qp5lm19',
+  test_user_id : '9d4702dc-e28d-4248-800a-03f093bec937',
+  region : 'us-east-2',
+  invoke_url : 'https://3saadq97v8.execute-api.us-east-2.amazonaws.com/prod'
 };
 
 export default creds;
